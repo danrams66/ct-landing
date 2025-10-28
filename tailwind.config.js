8c281b60fb967c1519f2cabc97448e7435eb6e6d
Your Name <you@example.com>
2025-10-27 21:38:15 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          blue: "#002B5B",
+          gold: "#C5A253",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
